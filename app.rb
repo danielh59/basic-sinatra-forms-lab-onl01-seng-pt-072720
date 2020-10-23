0f56d6cde440897e067e79961d5ccff973cbeab2
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-23 05:45:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-    @namee = params["name"]
-    @coach= params["coach"]
-    @pg= params["pg"]
-    @sg= params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @c = params["c"]
+    @namee = params[:name]
+    @coach= params["coach]
+    @pg= params[:pg]
+    @sg= params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 
